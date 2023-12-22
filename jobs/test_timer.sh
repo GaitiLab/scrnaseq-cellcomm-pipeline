57f3f1ac37e5a8f1ecd66948c39d0bceef974465
joan-yanqiong <j.b.y.kant@student.tue.nl>
2023-12-22 12:42:40 -0500

    clean up repo

--- jobs/test_timer.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-work_dir="/Users/joankant/Desktop/gaitigroup/Users/Joan/h4h-cell-cell-interactions/"
-
-source "$HOME/miniforge3/bin/activate" "standard_env"
-
-gtimeout 10 Rscript "$work_dir/scripts/test_timer.R" \
-    --sleep_time 100
-
-echo $?
